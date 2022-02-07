43afb9f55e2ac255f443a74ae421fcedaa204a15
Hi <Hi@DESKTOP-CR7BQJC>
2022-02-07 11:23:39 +0530

    Example of javaVariables

---
+++ 07-02-2022/src/com/javaVariables/Example1.java
@@ -0,0 +1,36 @@
+package com.javaVariables;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		//Write a java program to print student information using java variables and literals
+		
+		//variable declaration
+		int studentId;
+		String studentName;
+		float studentMarks;
+		String studentAddress;
+		
+		//variable initialization
+		studentId = 0550;
+		studentName = "Srikala";
+		studentMarks = 69.30f;
+		studentAddress = "Kakinada";
+		
+		System.out.print("Student Id: ");
+		System.out.println(studentId);
+		
+		System.out.print("Student Name: ");
+		System.out.println(studentName);
+		
+		System.out.print("Student Marks: ");
+		System.out.println(studentMarks);
+		
+		System.out.print("Student Address: ");
+		System.out.println(studentAddress);
+		
+		
+
+	}
+
+}

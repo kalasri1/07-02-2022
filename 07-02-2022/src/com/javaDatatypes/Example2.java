461b1c146c640c1fa1a29947a68bab0c610651ec
Hi <Hi@DESKTOP-CR7BQJC>
2022-02-07 12:04:33 +0530

    Example2 javaDatatypes

---
+++ 07-02-2022/src/com/javaDatatypes/Example2.java
@@ -0,0 +1,45 @@
+package com.javaDatatypes;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		//Write a java program to print student information(student id,name,age,marks,grade,result,address)
+		
+				//variable declaration
+				int studentId;
+				String studentName;
+				int studentAge;
+				float studentMarks;
+				char studentGrade;
+				boolean studentResult;
+				String studentAddress;
+				
+				//variable initialization
+				studentId = 0550;
+				studentName = "Srikala";
+				studentMarks = 69.35f;
+				studentGrade = 'A';
+				studentResult= true;
+				studentAddress="Kakinada";
+				
+				System.out.print("Student Id: ");
+				System.out.println(studentId);
+				
+				System.out.print("Student Name: ");
+				System.out.println(studentName);
+				
+				System.out.print("Student Marks: ");
+				System.out.println(studentMarks);
+				
+				System.out.print("Student Grade: ");
+				System.out.println(studentGrade);
+				
+				System.out.print("Student Result: ");
+				System.out.println(studentResult);
+				
+				System.out.print("Student Address: ");
+				System.out.println(studentAddress);
+
+	}
+
+}
